a44819967be35ba46152ac394e4038ab0f51ff5c
Aditya <addiegupta@gmail.com>
2018-07-25 15:57:20 +0530

    Add Finding middle element in a linked list

---
+++ Finding middle element in a linked list.cpp
@@ -0,0 +1,115 @@
+/*
+
+GeeksForGeeks: Finding middle element in a linked list
+
+Link to Problem: https://practice.geeksforgeeks.org/problems/finding-middle-element-in-a-linked-list/0
+
+Difficulty: Basic
+
+Given a singly linked list, find middle of the linked list. For example, if given linked list is 1->2->3->4->5 then output should be 3. 
+
+If there are even nodes, then there would be two middle nodes, we need to print second middle element. For example, if given linked list is 1->2->3->4->5->6 then output should be 4.
+
+Input:
+You have to complete the method which takes one argument: the head of the linked list. You should not read any input from stdin/console.
+The struct Node has a data part which stores the data and a next pointer which points to the next element of the linked list. 
+There are multiple test cases. For each test case, this method will be called individually.
+
+Output:
+Your function should return data of linked list.  If linked list is empty, then return -1.
+
+Constraints:
+1 <=T<= 100
+1 <=N<= 100
+1 <=value<= 1000
+
+Example:
+Input:
+2
+5
+1 2 3 4 5
+6
+2 4 6 7 5 1
+
+Output:
+3
+7
+
+*/
+
+{
+// Code is part of the question, only the function getMiddle is the solution
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+void append(struct Node** head_ref, struct Node **tail_ref, int new_data)
+{
+    struct Node* new_node = new Node;
+    new_node->data  = new_data;
+    new_node->next = NULL;
+    if (*head_ref == NULL)
+       *head_ref = new_node;
+    else
+       (*tail_ref)->next = new_node;
+    *tail_ref = new_node;
+}
+/* Function to get the middle of the linked list*/
+int getMiddle(struct Node *head);
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l;
+    cin>>T;
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            cin>>l;
+            append(&head, &tail, l);
+        }
+    printf("%d
+", getMiddle(head));
+    }
+    return 0;
+}
+
+}
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/* Link list Node 
+struct Node {
+    int data;
+    Node* next;
+}; */
+/* Should return data of middle node. If linked list is empty, then  -1*/
+int getMiddle(Node *head)
+{
+   // Your code here
+   if(head==NULL){
+       return -1;
+   }
+   struct Node* temp;
+   temp = head;
+   int l =0;
+   while(temp!=NULL){
+       l++;
+       temp= temp->next;
+   }
+   temp=head;
+   if(l%2==0) l++;
+   for(int i=0;i<(l/2);i++){
+       temp=temp->next;
+   }
+   return temp->data;
+}
